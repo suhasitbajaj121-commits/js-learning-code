29ebb05e0fc65e99135cb5c1ec79651e6def8dd8
suhasitbajaj121-commits <suhasitbajaj121@gmail.com>
2026-07-03 17:08:17 +0000

    container config done

---
+++ js_basics/lesson2_variable.js
@@ -0,0 +1,9 @@
+const accountId="14411"
+let userName="suhasit@gmail.com"
+var password="9898"
+accountCity="sikar"
+let accountStatus;
+
+userName="bajaj@gmail.com"
+password="7878"
+console.table([accountId,userName,password,accountCity,accountStatus])
---
+++ js_basics/lesson3_datatypes.js
@@ -0,0 +1,15 @@
+ //numbers = 12
+ // string ="suhasit"
+ //boolean= true or false
+ //null= stand alone
+ //undefiend
+ //object
+ //symbol= unique
+
+let name="suhasitbajaj"
+let number=838796222
+let age=true
+let sirName=null;
+let course;
+console.log(typeof course)
+//typeof is used to get the datatype of the variable

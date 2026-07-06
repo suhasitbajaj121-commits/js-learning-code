680a22da3bdb595d85af02831f83dab26f6f8a96
suhasitbajaj121-commits <suhasitbajaj121@gmail.com>
2026-07-06 16:41:36 +0000

    done

--- js_basics/lessson8_string.js
+++ js_basics/lessson8_string.js
@@ -11,16 +11,16 @@ let name = new String("Suhasit-Bajaj") //new is the keyword that will create the
 //console.log(typeof name) //here string is an object, and this object has some prototype
 //console.log(name.__proto__) //prototype of string , this shows that string is object and prototype syntax
 //String Method to know the string methods enter the let name = new String("Suhasit") in console it will show the string methods this methods is accessble using . 
-console.log(name.toUpperCase()) // () these are the function in the string methods
-console.log(name.toLowerCase())
-console.log(name.length)
-console.log(name.charAt(2)) // this show character at 2nd position
-console.log(name.indexOf('u')) // this show what is the index value of u
-console.log(name.substring(0,4))// do not accept negative value and always ignore the last number print 4 character but 0 to 3 index character
+//console.log(name.toUpperCase()) // () these are the function in the string methods
+//console.log(name.toLowerCase())
+//console.log(name.length)
+//console.log(name.charAt(2)) // this show character at 2nd position
+///console.log(name.indexOf('u')) // this show what is the index value of u
+console.log(name.substring(2,4))// do not accept negative value and always ignore the last number print 4 character but 0 to 3 index character
 console.log(name.slice(0,5))//accept negative value and always ignore the last number print 4 character but 0 to 3 index character
-console.log(name.trim()) //remove the space from the string 
-console.log(name.replace("S","A"))//replace the value or the character
-console.log(name.includes("q")) //show wether the value is present or not
-console.log(name.split("-"))// split the string according to the given conditions
-console.log(name.charCodeAt(4)) // this represent the code of the character access the character using the index numbber
-console.log(name.endsWith("Bajaj"))// shows wether the string ends with the define word or character output true and false
+//console.log(name.trim()) //remove the space from the string 
+//console.log(name.replace("S","A"))//replace the value or the character
+//console.log(name.includes("q")) //show wether the value is present or not
+//console.log(name.split("-"))// split the string according to the given conditions
+//console.log(name.charCodeAt(4)) // this represent the code of the character access the character using the index numbber
+//console.log(name.endsWith("Bajaj"))// shows wether the string ends with the define word or character output true and false
